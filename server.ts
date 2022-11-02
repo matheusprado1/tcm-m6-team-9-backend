876718a8bdae3db14d8fde8a2b4ff9f20a3de44e
matheusprado1 <matheusprado1096@gmail.com>
2022-11-02 14:56:58 -0300

    feat: configuration to connect to postgres database

---
+++ server.ts
@@ -0,0 +1,16 @@
+import AppDataSource from "./data-source";
+import app from "./app";
+import "dotenv/config";
+
+(async () => {
+  await AppDataSource.initialize()
+    .then(() => {
+      console.log("Database connection established");
+    })
+    .catch((error) => {
+      console.error("Database connection failed", error);
+    });
+  const port = process.env.PORT || 8080;
+
+  app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
+})();
